29f7d3f56878598bc987025337c6ce8952fc5012
yazan <yazan@DESKTOP-5FCPB2J>
2018-08-28 21:38:03 +0200

    MyVariables
    This is my first Variable

---
+++ MyVaribles/MyVaribles/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVaribles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int myValue =7 ;
+
+            Console.Write("{0}", myValue);
+        }
+    }
+}
