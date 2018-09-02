b02a56f802833efcb21e3caafd144aa304bcb0d5
yazan <yazan@DESKTOP-5FCPB2J>
2018-09-02 16:48:07 +0200

    Literal_Characters

---
+++ Literal_Characters/Literal_Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literal_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = -100;
+            uint myUVar = 1000U;
+
+            float myFlVar = 1.5F;
+            double myDVar = 100.5;
+            decimal myDecVar = 100.5m;
+
+            Console.WriteLine("{0} {1}", myVar, myUVar);
+            Console.WriteLine($"{myDecVar}, {myDVar}");
+            Console.ReadKey();
+
+        }
+    }
+}
