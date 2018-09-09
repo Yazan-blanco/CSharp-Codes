f4973f8e9f4bcc84cbe722dac98cc9ecb3a5df7c
yazan <yazan@DESKTOP-5FCPB2J>
2018-09-09 17:26:35 +0200

    StringType

---
+++ StringType_and_EscapeCharacters/StringType_and_EscapeCharacters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringType_and_EscapeCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // \' \" \\ \0 \a \b \n \r \t \v \f
+            // @ 
+
+            string myString = "Nazar said This is a good cours, and I think\0";
+            string myString2 = "Yes it's";
+            string myString3 = myString + myString2;
+
+
+            Console.WriteLine("{0}", myString3);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
